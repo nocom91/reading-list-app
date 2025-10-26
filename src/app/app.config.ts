f905311a488a0f082449c3388c67581b0dedbef6
Anastasiya Kostina <anastasiya.yu.kostina@gmail.com>
2025-10-26 11:49:09 +0500

    Add taiga ui, remove legacy code after Material installation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
+import { provideEventPlugins } from "@taiga-ui/event-plugins";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    providers: [
+        provideAnimations(),
+        provideBrowserGlobalErrorListeners(),
+        provideZonelessChangeDetection(),
+        provideRouter(routes),
+        provideEventPlugins()
+    ]
 };
