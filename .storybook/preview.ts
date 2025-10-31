ae565879b9bd310bac7ce9dac7f7d344703cd5a6
Anastasiya Kostina <anastasiya.yu.kostina@gmail.com>
2025-10-31 17:33:14 +0500

    Add TuiRoot as dependency to Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,20 @@
-import type { Preview } from '@storybook/angular'
+import { TuiRoot } from '@taiga-ui/core';
+import type { Preview } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
 
 const preview: Preview = {
+  decorators: [
+    moduleMetadata({
+      imports: [TuiRoot],
+      providers: [NG_EVENT_PLUGINS]
+    })
+  ],
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
