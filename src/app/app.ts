0e76863da9407b9e7c8b92aca68db32362f4d763
Anastasiya Kostina <anastasiya.yu.kostina@gmail.com>
2025-10-31 15:44:35 +0500

    Fix files with prettier

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { TuiRoot } from "@taiga-ui/core";
+import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'rl-root',
   imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('reading-list-app');
